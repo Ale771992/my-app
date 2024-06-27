131cf520a8953354e8a99dc0c23a3deeb1367ba2
Alejandra Rivera M <encuadernacionyali@gmail.com>
2024-06-27 14:29:17 -0600

    Tiene el codigo para definir el estado global en useStore.ts

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,5 +1,19 @@
-import create from 'zustand'
+import { create } from 'zustand'
 
-interface allItems {
-    
+interface Task {
+    id: number,
+    title: string,
+    completed: boolean
 }
+
+interface Store {
+    tasks: Task[];
+    addTask: (task: Task) => void;
+    setTasks: (tasks: Task[]) => void;
+}
+
+export const useStore = create<Store>((set) => ({
+    tasks: [],
+    addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
+    setTasks: (tasks) => set({ tasks })
+}))
